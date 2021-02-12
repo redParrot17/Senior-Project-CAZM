b36d5cedf2b45f2520206e4a1dd25d231ee2f390
Moriah <krausemj17@gcc.edu>
2021-02-11 23:53:01 -0500

    Finished making the advisor review page look nice/much better!

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 from flask import Flask,render_template
+
+''' set app, cache time, and session secret key '''
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 # no cache
+app.config["SECRET_KEY"] = "!kn4fs%dkl#JED*BKS89" # Secret Key for Sessions
 
 @app.route('/')
 def index():
@@ -11,4 +15,39 @@ def login():
 
 @app.route('/advisorSchReview/')
 def advisorSchReview():
-    return render_template('advisorStudentScheduleReview.html')
+    classes=["Fall 2021","Spring 2022","Fall 2022","Spring 2021"]
+    statusSheet=[
+	{
+		"title":"Humanities Core",
+		"classes": ["c1", "c2", "c3"]
+
+	},
+    {
+		"title":"SSFT Requirement",
+		"classes": ["c1", "c2", "c3"]
+
+	},
+    {
+		"title":"Writing Requirement",
+		"classes": ["c1", "c2", "c3"]
+
+	},
+    {
+		"title":"Foundations of Social Sciences",
+		"classes": ["c1", "c2", "c3"]
+
+	},
+	{
+		"title":"Physical Education",
+		"classes": ["c1", "c2", "c3"]
+
+	}
+]
+    return render_template('advisorStudentScheduleReview.html', classes=classes, statusSheet=statusSheet)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# Having debug=True allows possible Python errors to appear on the web page
+# run with $> python server.py
