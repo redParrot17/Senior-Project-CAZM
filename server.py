1f60a8f0bceea1d9807b406a5ef1059af2b697ad
Moriah <krausemj17@gcc.edu>
2021-02-13 16:07:21 -0500

    login updates

--- server.py
+++ server.py
@@ -1,9 +1,31 @@
+import flask
 from flask import Flask,render_template
-
+import flask_login
+from flask_login import login_required
+from os import urandom
+from user import User
+from mygcc import MyGcc
 ''' set app, cache time, and session secret key '''
+
+#users dictionary
+user = {}
+
+
 app = Flask(__name__)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 # no cache
-app.config["SECRET_KEY"] = "!kn4fs%dkl#JED*BKS89" # Secret Key for Sessions
+app.secret_key = urandom(16)
+
+#app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 # no cache
+#app.config["SECRET_KEY"] = "!kn4fs%dkl#JED*BKS89" # Secret Key for Sessions
+
+#set up login manager
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+#convert user id to a user object
+@login_manager.user_loader
+def load_user(user_id):
+    return users.get(user_id)
+
 
 @app.route('/')
 def index():
@@ -17,7 +39,25 @@ def login_get():
 
 @app.route('/login/', methods=['POST'])
 def login_post():
-
+    username=flask.request.form['username']
+    password=flask.request.form['password']
+    mygcc = MyGcc(username, password)
+    try:
+        mygcc.login()
+        user_id = mygcc.profile.user_id
+        is_advisor = mygcc.advising.is_advisor
+        if user_id is not None:
+            user = User(user_id, is_advisor, username, password)
+            users[user.get_id()] = users
+            flask_login.login_user(user)
+            if is_advisor:
+                return flask.redirect(flask.url_for("advisorHomePreview"))
+            else:
+                return flask.redirect(flask.url_for("advisorHomePreview"))
+        else:
+            print('Bad Login')
+    except Exception:
+        print('Bad Login')
 	#TODO Replace this route handler with proper login credential handling
     return render_template('loginPage.html')
 
@@ -64,84 +104,6 @@ def advisorHomePreview():
         {'id': 208776, 'name': 'Shel Silverstein', 'credits': 47, 'email': 'example@gcc.edu', 'status': 4, 'year': 'Freshman'},
     ]
     return render_template('advisorLandingPage.html', advisees=advisees)
-
-@app.route('/studentLanding')
-def studentLanding():
-	student = [ {'id': 209123, 'name': 'Sally Silly', 'credits': 54, 'status': 'Pending', 'grad_semester': 'Spring 2024', 'major': 'Computer Science'} ]
-	studentSchedule = [
-		{
-			'semester': 'Fall 2020',
-			'classes': ['COMP 141', 'COMP 155', 'MATH 161', 'PHYS 101', 'HUMA 102']
-		},
-		{
-			'semester': 'Spring 2021',
-			'classes': ['COMP 220', 'MATH 162', 'PHYS 102', 'WRIT 101', 'PHYE 100']
-		},
-		{
-			'semester': 'Fall 2021',
-			'classes': ['COMP 222', 'COMP 244', 'MATH 213', 'HUMA 200', 'PSYC 101', 'General Elective']
-		},
-		{
-			'semester': 'Spring 2022',
-			'classes': ['COMP 205', 'COMP 233', 'COMP 342', 'MATH 214', 'HUMA 202']
-		},
-		{
-			'semester': 'Fall 2022',
-			'classes': ['COMP 325', 'COMP 422', 'COMP 390', 'COMP 401', 'HUMA 301', 'General Elective']
-		},
-		{
-			'semester': 'Spring 2023',
-			'classes': ['COMP 314', 'COMP 340', 'COMP 350', 'COMP 402', 'General Elective', 'General Elective']
-		},
-		{
-			'semester': 'Fall 2023',
-			'classes': ['COMP 448', 'COMP 451', 'COMP 435', 'HUMA 303', 'General Elective', 'General Elective']
-		},
-		{
-			'semester': 'Spring 2024',
-			'classes': ['COMP 443', 'COMP 452', 'COMP 441', 'General Elective', 'General Elective']
-		}
-	]
-	return render_template('studentLanding.html', student=student, studentSchedule=studentSchedule)
-
-@app.route("/studentProfile/")
-def advisorViewingStudent():
-	student = [ {'id': 209123, 'name': 'Sally Silly', 'credits': 54, 'status': 'Pending', 'grad_semester': 'Spring 2024', 'major': 'Computer Science'} ]
-	studentSchedule = [
-		{
-			'semester': 'Fall 2020',
-			'classes': ['COMP 141', 'COMP 155', 'MATH 161', 'PHYS 101', 'HUMA 102']
-		},
-		{
-			'semester': 'Spring 2021',
-			'classes': ['COMP 220', 'MATH 162', 'PHYS 102', 'WRIT 101', 'PHYE 100']
-		},
-		{
-			'semester': 'Fall 2021',
-			'classes': ['COMP 222', 'COMP 244', 'MATH 213', 'HUMA 200', 'PSYC 101', 'General Elective']
-		},
-		{
-			'semester': 'Spring 2022',
-			'classes': ['COMP 205', 'COMP 233', 'COMP 342', 'MATH 214', 'HUMA 202']
-		},
-		{
-			'semester': 'Fall 2022',
-			'classes': ['COMP 325', 'COMP 422', 'COMP 390', 'COMP 401', 'HUMA 301', 'General Elective']
-		},
-		{
-			'semester': 'Spring 2023',
-			'classes': ['COMP 314', 'COMP 340', 'COMP 350', 'COMP 402', 'General Elective', 'General Elective']
-		},
-		{
-			'semester': 'Fall 2023',
-			'classes': ['COMP 448', 'COMP 451', 'COMP 435', 'HUMA 303', 'General Elective', 'General Elective']
-		},
-		{
-			'semester': 'Spring 2024',
-			'classes': ['COMP 443', 'COMP 452', 'COMP 441', 'General Elective', 'General Elective']
-		}
-	]
-	return render_template('advisorViewingStudent.html', student=student, studentSchedule=studentSchedule)
 
 
 if __name__ == "__main__":
---
+++ user.py
@@ -0,0 +1,17 @@
+from flask_login import UserMixin
+
+class User(UserMixin):
+
+    def __init__(self, user_id, is_advisor, username, password):
+        self.id = user.id
+        self.is_advisor = is_advisor
+        self.username = username
+        self.password = password
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
