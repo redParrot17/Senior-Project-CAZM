3bebbd0ea60ffa27c44a7fa544f732eeb243b85e
Zack Orlaski <zorlaski@gmail.com>
2021-02-15 22:03:27 -0500

    Alternate Course Search

--- static/js/searchcourses.js
+++ static/js/searchcourses.js
@@ -1,16 +1,57 @@
 var classes;
+
+
+
 window.addEventListener("DOMContentLoaded", function () {
 
     let search = document.getElementById("search-button");
 
-    search.addEventListener("click", function(){
+    search.addEventListener("click", function () {
         $.getJSON($SCRIPT_ROOT + '/searchClasses', {
             class_name: $('input[id="search-bar"]').val()
-         }, function(data) {
-             classes = data
-             console.log(classes)
-             console.log("--Search Done--");
-         });
+        }, function (data) {
+            classes = data
+            console.log(classes)
+            console.log("--Search Done--");
+            let container = document.getElementById("course-container");
+            container.innerHTML = "";
+            for (let i in classes) {
+                let course = classes[i]
+
+                /* Outer Div */
+                let name = course.course_code;
+                          
+                 let id = name.replace(" ", "_");
+                let div = document.createElement("div");
+                div.id = id;
+                div.className = "drag_item";
+                div.draggable = "true";
+                div.setAttribute("ondragstart", "drag(event)");
+                div.setAttribute("ondragend", "dragEnd(event)");
+
+                /* Arrows icon */
+                let arrows = document.createElement("i");
+                arrows.classList = "fas fa-arrows-alt";
+
+                /* Class Text */
+                let span = document.createElement("span");
+                span.classList = "drag_item_text text-center";
+                span.innerHTML = name;
+
+                /* Trash Can */
+                let trash = document.createElement("i");
+                trash.classList = "fas fa-trash-alt ml-auto m-2 itemInvisible trashRed"
+                trash.setAttribute("onclick","removeDragItem(event)");
+
+                /* Combine elements */
+
+                div.appendChild(arrows)
+                div.appendChild(span)
+                div.appendChild(trash)
+                container.appendChild(div)
+
+            }
+        });
     });
 
 
