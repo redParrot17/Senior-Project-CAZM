5014644f8aa5f879f6e002cf730d2cd16b696fd1
Moriah <krausemj17@gcc.edu>
2021-03-01 15:16:49 -0500

    one more baby fix

--- static/js/searchcourses.js
+++ static/js/searchcourses.js
@@ -14,7 +14,7 @@ window.addEventListener("DOMContentLoaded", function () {
         reqs = data;
         console.log(reqs);
         loadStatusSheet(reqs);
-        
+
     })
 
 
@@ -45,16 +45,16 @@ window.addEventListener("DOMContentLoaded", function () {
 
                 /* Arrows icon */
                 let arrows = document.createElement("i");
-                arrows.classList = "fas fa-arrows-alt";
+                arrows.classList = "col-1 pr-0 pl-0 ml-2 fas fa-arrows-alt";
 
                 /* Class Text */
                 let span = document.createElement("span");
-                span.classList = "drag_item_text text-center";
+                span.classList = "col pr-0 pl-0 drag_item_text text-center";
                 span.innerHTML = name;
 
                 /* Trash Can */
                 let trash = document.createElement("i");
-                trash.classList = "fas fa-trash-alt ml-auto m-2 itemInvisible trashRed"
+                trash.classList = "col-1 pr-0 pl-0 fas fa-trash-alt ml-auto m-2 itemInvisible trashRed"
                 trash.setAttribute("onclick", "removeDragItem(event)");
 
                 /* Combine elements */
