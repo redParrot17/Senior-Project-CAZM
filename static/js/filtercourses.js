21f5136070d2e41486011cb6732f79e2c8ec9ab0
MoonTreeInteractive <alecmuchnok@gmail.com>
2021-03-13 23:45:16 -0500

    semester filter now shows courses from any semester when 'Semester:' option is chosen in dropdown

--- static/js/filtercourses.js
+++ static/js/filtercourses.js
@@ -30,56 +30,7 @@ function filterDuplicates(checkbox) {
             }
 
             /* Outer Div */
-          let name = course.course_code;
-
-          let courseID = name/*.replace(" ", "_") + "_" + course.semester + "_" + course.year*/;
-          let div = document.createElement("div");
-          div.id = courseID;
-          div.className = "drag_item";
-          div.draggable = "true";
-
-          div.setAttribute("semester",course.semester);
-          div.setAttribute("courseCode",courseID);
-          div.setAttribute("ondragstart", "set_valid_drag_locations(event);drag(event);");
-          div.setAttribute("ondragend", "revert_drag_locations(event);dragEnd(event);");
-
-          /* Arrows icon */
-          let arrows = document.createElement("i");
-          arrows.classList = "col-1 pr-0 pl-0 ml-2 fas fa-arrows-alt";
-
-          /* Class Text */
-          let span = document.createElement("span");
-          span.classList = "col pr-0 pl-0 drag_item_text text-center";
-          span.innerHTML = name;
-
-          /* Trash Can */
-          let trash = document.createElement("i");
-          trash.classList = "col-1 pr-0 pl-0 fas fa-trash-alt ml-auto m-2 itemInvisible trashRed"
-          trash.setAttribute("onclick", "removeDragItem(event)");
-
-          /* Combine elements */
-
-          div.appendChild(arrows)
-          div.appendChild(span)
-          div.appendChild(trash)
-          container.appendChild(div)
-        }
-    });
-}
-
-function filterSemester(semesterDropdown) {
-    $.getJSON($SCRIPT_ROOT + '/filterSemester', {
-        semester: semesterDropdown.value
-    }, function (data, status) {
-        classes = data
-
-        let container = document.getElementById("course-container");
-        container.innerHTML = "";
-        for (let i in classes) {
-            let course = classes[i]
-
-            /* Outer Div */
-            let name = course.courseCode;
+            let name = course.course_code;
 
             let courseID = name/*.replace(" ", "_") + "_" + course.semester + "_" + course.year*/;
             let div = document.createElement("div");
@@ -114,4 +65,102 @@ function filterSemester(semesterDropdown) {
             container.appendChild(div)
         }
     });
+}
+
+function filterSemester(semesterDropdown) {
+    if (semesterDropdown.value != -1) {
+        $.getJSON($SCRIPT_ROOT + '/filterSemester', {
+            semester: semesterDropdown.value
+        }, function (data, status) {
+            classes = data
+
+            let container = document.getElementById("course-container");
+            container.innerHTML = "";
+            for (let i in classes) {
+                let course = classes[i]
+
+                /* Outer Div */
+                let name = course.courseCode;
+
+                let courseID = name/*.replace(" ", "_") + "_" + course.semester + "_" + course.year*/;
+                let div = document.createElement("div");
+                div.id = courseID;
+                div.className = "drag_item";
+                div.draggable = "true";
+
+                div.setAttribute("semester", course.semester);
+                div.setAttribute("courseCode", courseID);
+                div.setAttribute("ondragstart", "set_valid_drag_locations(event);drag(event);");
+                div.setAttribute("ondragend", "revert_drag_locations(event);dragEnd(event);");
+
+                /* Arrows icon */
+                let arrows = document.createElement("i");
+                arrows.classList = "col-1 pr-0 pl-0 ml-2 fas fa-arrows-alt";
+
+                /* Class Text */
+                let span = document.createElement("span");
+                span.classList = "col pr-0 pl-0 drag_item_text text-center";
+                span.innerHTML = name;
+
+                /* Trash Can */
+                let trash = document.createElement("i");
+                trash.classList = "col-1 pr-0 pl-0 fas fa-trash-alt ml-auto m-2 itemInvisible trashRed"
+                trash.setAttribute("onclick", "removeDragItem(event)");
+
+                /* Combine elements */
+
+                div.appendChild(arrows)
+                div.appendChild(span)
+                div.appendChild(trash)
+                container.appendChild(div)
+            }
+        });
+    }
+    else {
+        $.getJSON($SCRIPT_ROOT + '/searchClasses', {
+            class_name: $('input[id="search-bar"]').val()
+        }, function (data, status) {
+            classes = data
+            let container = document.getElementById("course-container");
+            container.innerHTML = "";
+            for (let i in classes) {
+                let course = classes[i]
+
+                /* Outer Div */
+                let name = course.course_code;
+
+                let courseID = name/*.replace(" ", "_") + "_" + course.semester + "_" + course.year*/;
+                let div = document.createElement("div");
+                div.id = courseID;
+                div.className = "drag_item";
+                div.draggable = "true";
+
+                div.setAttribute("semester", course.semester);
+                div.setAttribute("courseCode", courseID);
+                div.setAttribute("ondragstart", "set_valid_drag_locations(event);drag(event);");
+                div.setAttribute("ondragend", "revert_drag_locations(event);dragEnd(event);");
+
+                /* Arrows icon */
+                let arrows = document.createElement("i");
+                arrows.classList = "col-1 pr-0 pl-0 ml-2 fas fa-arrows-alt";
+
+                /* Class Text */
+                let span = document.createElement("span");
+                span.classList = "col pr-0 pl-0 drag_item_text text-center";
+                span.innerHTML = name;
+
+                /* Trash Can */
+                let trash = document.createElement("i");
+                trash.classList = "col-1 pr-0 pl-0 fas fa-trash-alt ml-auto m-2 itemInvisible trashRed"
+                trash.setAttribute("onclick", "removeDragItem(event)");
+
+                /* Combine elements */
+
+                div.appendChild(arrows)
+                div.appendChild(span)
+                div.appendChild(trash)
+                container.appendChild(div)
+            }
+        });
+    }
 }
