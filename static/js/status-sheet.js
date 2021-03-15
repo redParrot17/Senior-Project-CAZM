ef5b95c3657ccf231f4bf04cda39d53e04223fdc
Zack Orlaski <zorlaski@gmail.com>
2021-03-14 23:11:16 -0400

    Credits appear in status sheet

--- static/js/status-sheet.js
+++ static/js/status-sheet.js
@@ -5,6 +5,30 @@ curSemester = {
 }
 
 
+/* Gets number of credits for a coursecode */
+function getCredits(code) {
+	
+	var returnedCredits = -1;
+
+	let found = false;
+	var courseIndex = 0;
+	
+	while (!found && courseIndex < listOfCourses.length) {
+		let searchedCourse = listOfCourses[courseIndex];
+
+		if (searchedCourse.courseCode === code[0]) {
+			found = true;
+			returnedCredits = searchedCourse.credits;
+		}
+		courseIndex++;
+	}
+
+	
+	return returnedCredits;
+}
+
+
+
 
 function loadStatusSheet(reqs) {
 	let keys = Object.keys(reqs)
@@ -17,7 +41,7 @@ function loadStatusSheet(reqs) {
 
 		let legend = document.createElement("legend")
 		legend.classList = "StatusSheetTitles"
-		legend.innerText = value.title.replaceAll("_"," ")
+		legend.innerText = value.title.replaceAll("_", " ")
 
 		fieldset.appendChild(legend)
 
@@ -25,24 +49,46 @@ function loadStatusSheet(reqs) {
 		container.appendChild(fieldset)
 
 		let courselist = document.createElement("ul")
-		courselist.className="fa-ul"
+		courselist.className = "fa-ul"
 		var courseAry = value.classes
 
 		for (let i in courseAry) {
 
+
 			let li = document.createElement("li")
 			li.classList = "requirement-list-course"
-			li.innerHTML = courseAry[i]
 
+			let code = courseAry[i]
+			li.innerHTML = code
+
+
+			/*Requirement Credits*/
+			let creditSpan = document.createElement("span") ;
+			let creds = getCredits(code)
+			if(creds !== -1){
+				creditSpan.innerText = " - " + creds + " Credits";
+			}
+			else{
+				creditSpan.innerText = " - Course Unavailable!";
+			}
+
+
+
+
+
+
+
+			/* Checkbox */
 			let span = document.createElement("span")
 			span.innerHTML = '<i class="far fa-square" style="color: red;"></i>'
-			span.className="fa-li"
+			span.className = "fa-li"
 			li.appendChild(span)
-	
-
+			li.appendChild(creditSpan)
 			courselist.appendChild(li)
 
 		}
+
+
 
 
 		fieldset.appendChild(courselist)
@@ -56,73 +102,92 @@ function loadStatusSheet(reqs) {
 			or.innerHTML = "OR"
 			or.style = "font-weight: bold; font-size: 1.1em"
 			fieldset.appendChild(or)
-			let alt = reqs[alt_req_key].classes
+			let altCourseAry = reqs[alt_req_key].classes
 
 
 			let altCourselist = document.createElement("ul")
-			altCourselist.className="fa-ul"
-			let altCourseAry = alt
+			altCourselist.className = "fa-ul"
+		
 
 			for (let i in altCourseAry) {
 
 				let li = document.createElement("li")
 				li.classList = "requirement-list-course"
-				li.innerHTML = altCourseAry[i]
+				let code = altCourseAry[i]
+				li.innerHTML = code
+
+
+				/*Requirement Credits*/
+				let creditSpan = document.createElement("span");
+
+				let creds = getCredits(code)
+				if(creds !== -1){
+					creditSpan.innerText = " - " + creds + " Credits";
+				}
+				else{
+					creditSpan.innerText = "Course Unavailable!";
+				}
+	
+
+
 
 				let span = document.createElement("span")
 				span.innerHTML = '<i class="far fa-square" style="color: red;"></i>'
-				span.className="fa-li"
+				span.className = "fa-li"
 				li.appendChild(span)
-		
+				li.appendChild(creditSpan)
 
 				altCourselist.appendChild(li)
 
-				
-				
+
+
 
 			}
 			fieldset.appendChild(altCourselist)
 
 			//REMOVE KEY FROM LIST
-			for(let k in keys){ 
-    
-				if ( keys[k] == alt_req_key) { 	
-					
-					keys.splice(k, 1); 
+			for (let k in keys) {
+
+				if (keys[k] == alt_req_key) {
+
+					keys.splice(k, 1);
 				}
-			
+
 			}
-		
+
 
 		} //End Alternates
 	}
 }
 
-function updateStatusSheet(selectedCourses){
+function updateStatusSheet(selectedCourses) {
 	let statusSheetCourses = document.getElementsByClassName("requirement-list-course")
+	console.log(selectedCourses)
 	let selectedClasses = document.getElementsByClassName("drag_item drag_item_fill")
-	for(let courseNum = 0; courseNum < statusSheetCourses.length; courseNum++){
+	for (let courseNum = 0; courseNum < statusSheetCourses.length; courseNum++) {
 		let ssCourse = statusSheetCourses[courseNum];
 		let ssCode = ssCourse.innerText;
-		for(let classNum = 0; classNum < selectedClasses.length; classNum++){
-			if(selectedClasses[classNum].getAttribute("coursecode").includes(ssCode)){
+		for (let classNum = 0; classNum < selectedClasses.length; classNum++) {
+			if (selectedClasses[classNum].getAttribute("coursecode").includes(ssCode)) {
 				let year = selectedClasses[classNum].getAttribute("year")
 				let semesterIndex = ALL_SEMESTERS.indexOf(selectedClasses[classNum].getAttribute("semester"))
-				if( ( year < curSemester.year) || (year === curSemester.year && semesterIndex <=  ALL_SEMESTERS.indexOf(curSemester.semester) ) ){
+				if ((year < curSemester.year) || (year === curSemester.year && semesterIndex <= ALL_SEMESTERS.indexOf(curSemester.semester))) {
+					console.log(year)
+					console.log(year)
 					ssCourse.childNodes[1].childNodes[0].classList = "fas fa-check-square"
 					ssCourse.childNodes[1].childNodes[0].style = "color: green;"
 				}
-				else{
-					
+				else {
+
 					ssCourse.childNodes[1].childNodes[0].classList = "fas fa-hourglass-start"
 					ssCourse.childNodes[1].childNodes[0].style = "color: blue;"
 				}
+				console.log(ssCourse.childNodes)
 
 
-	
 			}
 			// let selectedSemester = ALL_SEMESTERS.indexOf(selectedClasses[classNum].getAttribute("semester"))
-			
+
 
 		}
 
