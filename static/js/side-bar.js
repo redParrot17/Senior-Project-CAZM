d4d98cbcf82822652f082b76dcf9f2596e9a17a0
Zack Orlaski <zorlaski@gmail.com>
2021-03-23 19:30:16 -0400

    refactoring if div create

--- static/js/side-bar.js
+++ static/js/side-bar.js
@@ -42,40 +42,7 @@ function searchClasses() {
       container.innerHTML = "";
       for (let i in classes) {
           let course = classes[i]
-
-          /* Outer Div */
-          let name = course.course_code;
-
-          let courseID = name/*.replace(" ", "_") + "_" + course.semester + "_" + course.year*/;
-          let div = document.createElement("div");
-          div.id = courseID;
-          div.className = "drag_item";
-          div.draggable = "true";
-
-          div.setAttribute("semester",course.semester);
-          div.setAttribute("courseCode",courseID);
-          div.setAttribute("ondragstart", "set_valid_drag_locations(event);drag(event);");
-          div.setAttribute("ondragend", "revert_drag_locations(event);dragEnd(event);");
-
-          /* Arrows icon */
-          let arrows = document.createElement("i");
-          arrows.classList = "col-1 pr-0 pl-0 ml-2 fas fa-arrows-alt";
-
-          /* Class Text */
-          let span = document.createElement("span");
-          span.classList = "col pr-0 pl-0 drag_item_text text-center";
-          span.innerHTML = name;
-
-          /* Trash Can */
-          let trash = document.createElement("i");
-          trash.classList = "col-1 pr-0 pl-0 fas fa-trash-alt ml-auto m-2 itemInvisible trashRed"
-          trash.setAttribute("onclick", "removeDragItem(event)");
-
-          /* Combine elements */
-
-          div.appendChild(arrows)
-          div.appendChild(span)
-          div.appendChild(trash)
+          let div = createCourseDiv(course)
           container.appendChild(div)
 
       }
