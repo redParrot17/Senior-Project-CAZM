e9c501827252d942f9e882d91cf47afb76d8aac4
redParrot17 <44844267+redParrot17@users.noreply.github.com>
2021-03-24 15:30:32 -0400

    Fixed advisor edit schedule page so that it works for advisees now.

---
+++ static/js/AdviseeScheduleLoad.js
@@ -0,0 +1,57 @@
+var reqs;
+var requisites;
+var studentData;
+
+function createEventListeners(studentID) {
+    window.addEventListener("DOMContentLoaded", function () {
+
+        $.getJSON($SCRIPT_ROOT + '/adviseeData?student_id=' + studentID, {
+
+        }, function (data) {
+            studentData = data;
+
+            //Get requirements list for student's major
+            $.getJSON($SCRIPT_ROOT + '/getRequirements', {
+                'id' : studentData.id
+            }, function (reqData) {
+                reqs = reqData;
+                loadStatusSheet(reqs);
+            });
+
+            // set up student schedule containers
+            setUpStudentScheduleContainers(studentData);
+            addClassestoPools();
+            // updateStatusSheet();
+
+            //Get requisite list for each course
+            $.getJSON($SCRIPT_ROOT + '/getRequisites', {
+
+            }, function (requisiteData) {
+                requisites = requisiteData;
+                checkPools(false);
+            })
+
+        }, function (requisiteData) {
+            requisites = requisiteData;
+            addClassestoPools();
+            checkPools(false);
+        });
+
+    });
+
+
+    /**
+    FOR SEARCH FILTER ON SIDEBAR
+    */
+    // Get the input field
+    let input = document.getElementById("search-bar");
+    // Execute a function when the user releases a key on the keyboard
+    input.addEventListener("keyup", function(event) {
+
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById("search-button").click();
+
+    });
+}
